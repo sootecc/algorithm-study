565deb5f96f67fa3eb764e068324d81d49d473ba
sootecc <sootecc@gmail.com>
2022-04-26 12:41:33 +0900

    Ex5 sol

---
+++ String/Ex6.java
@@ -0,0 +1,20 @@
+package String;
+
+import java.util.Scanner;
+
+public class Ex6 {
+
+	public String solution(String sentence) {
+		
+		return answer;
+		
+	}
+	
+	public static void main(String[] args) {
+		Ex6 T = new Ex6();
+		Scanner sc = new Scanner(System.in);
+		String sentence = sc.next();
+		System.out.println(T.solution(sentence));
+	}
+
+}
