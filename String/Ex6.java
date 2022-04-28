3426a6ce172a99ed708474875ed60d71170b8951
sootecc <sootecc@gmail.com>
2022-04-28 22:01:22 +0900

    Ex6

--- String/Ex6.java
+++ String/Ex6.java
@@ -4,8 +4,27 @@ import java.util.Scanner;
 
 public class Ex6 {
 
-	public String solution(String sentence) {
+	public String solution(String str) {
+		String answer="";
+		int lt =0, rt = lt+1;
+		char[] tmp = str.toCharArray();
 		
+		
+		for(int i=lt; i<tmp.length-1;i++) {	
+			rt=i+1;
+			for(int j =rt; j<tmp.length;j++) {
+				if(tmp[i]==tmp[j]) {
+					tmp[j] = '1';
+				}
+			}
+			
+		}
+
+		
+		for(int i=0;i<tmp.length;i++) {
+			answer += tmp[i];
+		}
+		answer = answer.replaceAll("1", "");
 		return answer;
 		
 	}
