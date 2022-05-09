db9e4d6452c737c29f5af1abeed6196de811b77b
sootecc <sootecc@gmail.com>
2022-05-09 18:29:53 +0900

    Ex12

---
+++ String/Ex12.java
@@ -0,0 +1,45 @@
+package String;
+
+import java.util.Scanner;
+
+public class Ex12 {
+
+	public String solution(int count, String str) {
+		
+		String answer = "";
+		// # -> 1, * ->0
+		int[] sub = new int[7];
+		String[] result = new String[count]; 
+		String[] result2 = new String[count];
+		//substring ÀÌ¿כ
+		for(int i = 0 ; i< count;i++) {
+			result[i] = str.substring(7*i,7*i+7);
+			System.out.println(result[i]);
+		}
+		for(int i = 0 ; i< count ; i++) {
+			for(int j= 0 ;j<count ; j++) {
+				if(result[i].charAt(j) == '#') 
+					result2[i]+= 1;
+				else(result[i].charAt(j) == '*')
+					result2[i] += 0;
+			}
+		}
+		
+		
+		
+			
+		return answer;
+		
+	}
+	
+	public static void main(String[] args) {
+		Ex12 T = new Ex12();
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		String sentence = sc.next();
+		System.out.print(T.solution(count,sentence));
+		
+		
+	}
+
+}
